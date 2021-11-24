602e02f34fc434d4bd63bffb08186ef2d90bcf99
RabihSouiden <62447715+RabihSouiden@users.noreply.github.com>
2021-11-23 22:06:47 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ let formulario_ingreso = document.querySelector(".formulario-ingreso");
 let formulario_registro = document.querySelector(".formulario-registro");
 let contenedor_ingreso_registro = document.querySelector(".contenedor-ingreso-registro");
 let caja_posterior_ingreso = document.querySelector(".caja-posterior-ingreso");
-let caja_posterior_registro = document.querySelector(".caja-posterior-registro"); 
+let caja_posterior_registro = document.querySelector(".caja-posterior-registro");
 
 let registrar = document.getElementById("registro")
 
 //FUNCIONES
-function iniciarSesion(){
+function iniciarSesion() {
     event.preventDefault();
 }
 
@@ -74,38 +74,105 @@ function registrarse() {
 
 
 
-function registerData(){
-            console.log(document.getElementById("regisName").value)
-            let userToSave = {
-                'name': document.getElementById("regisName").value,
-                'mail': document.getElementById("regisMail").value,
-                'pssw': document.getElementById("regisPssw").value
-            }
-            let jsonUser = JSON.stringify(userToSave);
-            localStorage.setItem('user', jsonUser);
-            console.log('this is  a test');
+function registerData() {
+    console.log(document.getElementById("regisName").value)
+    let userToSave = {
+        'name': document.getElementById("regisName").value,
+        'mail': document.getElementById("regisMail").value,
+        'pssw': document.getElementById("regisPssw").value
+    }
+    let jsonUser = JSON.stringify(userToSave);
+    localStorage.setItem('user', jsonUser);
+    console.log('this is  a test');
+}
+
+
+
+
+function login() {
+    event.preventDefault();
+    let storeUser = JSON.parse(localStorage.getItem('user'));
+    console.log(document.getElementById("loginMail").value)
+    console.log(storeUser.mail)
+    if (document.getElementById("loginMail").value == storeUser.mail) {
+
+        if (document.getElementById("loginPssw").value == storeUser.pssw) {
+            window.location.href = "mapa.html";
+        } else {
+            alert('Contraseña incorrecta');
         }
+        console.log("TEST LOGIN")
+    } else {
+        alert('Usuario Incorrecto');
+    }
+}
 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
+    });
+    var input = document.getElementById('searchInput');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
 
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
 
-        function login(){
-            event.preventDefault();
-            var storeUser = JSON.parse(localStorage.getItem('user'));
-            console.log(document.getElementById("loginMail").value)
-            console.log(storeUser.mail)
-            if( document.getElementById("loginMail").value == storeUser.mail){
-
-                if(document.getElementById("loginPssw").value == storeUser.pssw){
-                    window.location.href = "mapa.html";
-                }else{
-                    alert('Contraseña incorrecta');
-                }
-                console.log("TEST LOGIN")
-            }else{
-                alert('Usuario Incorrecto');
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+  
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+        marker.setIcon(({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+    
+        var address = '';
+        if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+    
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+      
+        // Location details
+        for (var i = 0; i < place.address_components.length; i++) {
+            if(place.address_components[i].types[0] == 'postal_code'){
+                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+            }
+            if(place.address_components[i].types[0] == 'country'){
+                document.getElementById('country').innerHTML = place.address_components[i].long_name;
             }
         }
-
-
-
+        document.getElementById('location').innerHTML = place.formatted_address;
+        document.getElementById('lat').innerHTML = place.geometry.location.lat();
+        document.getElementById('lon').innerHTML = place.geometry.location.lng();
+    });
+}
